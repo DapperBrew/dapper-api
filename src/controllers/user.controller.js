4209a1b3a9ff8ddcb50ad7dab0fcb8a834f3e2f7
Dan Brown <slightlyoffbeat@gmail.com>
2017-03-04 01:32:44 -0800

    requires new signups to be on approved list. New signups tagged as "beta"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import bluebird from 'bluebird';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model';
+import Approved from '../models/approved.model';
 
 const tokenForUser = (user) => {
   const timestamp = new Date().getTime();
@@ -19,17 +20,40 @@ export const signup = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  // TEMP CODE
+  // return true if user is on the approved list
+  let approvedUser;
+  Approved.findOne({ email })
+    .then((approvedEmail) => {
+      console.log('approved email', approvedEmail);
+      if (approvedEmail) {
+        approvedUser = true;
+      } else {
+        approvedUser = false;
+      }
+      console.log('approved user', approvedUser);
+    });
+
   if (!email || !password) {
     return res.status(422).send({ error: 'You must supply both an email and a password' });
   }
 
+
   // See if user with given email exists
   User.findOne({ email })
     .then((existingUser) => {
+      // TEMP
+      // only allow if user is on approved list
+      if (approvedUser === false) {
+        return res.status(422).send({ error: 'Email address is not approved. Please contact hello@dapper.com for an invite' });
+      }
+
       // If user already exists, return an error
       if (existingUser) {
         return res.status(422).send({ error: 'That email is registered to an existing account' });
       }
+
+
       // If a user does NOT exist, create and save user record
       const user = new User({
         email,
