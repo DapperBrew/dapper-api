0559be5b865caeaf8b4f5e7d2c3c0eff46c7d3f0
Dan Brown <slightlyoffbeat@gmail.com>
2018-07-26 01:10:01 -0700

    updating npm packages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import 'babel-polyfill';
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import expressValidator from 'express-validator';
+import "babel-polyfill";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import expressValidator from "express-validator";
 
-import 'dotenv/config';
+import "dotenv/config";
 
-import initializeDb from './db';
-import middleware from './middleware';
-import routes from './routes';
-import config from './config.json';
+import initializeDb from "./db";
+import middleware from "./middleware";
+import routes from "./routes";
+import config from "./config.json";
 
 const app = express();
 app.server = http.createServer(app);
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: config.bodyLimit }));
 
 // connect to db
-initializeDb((db) => {
+initializeDb(db => {
   // internal middleware
   app.use(middleware({ config, db }));
 
@@ -30,10 +30,10 @@ initializeDb((db) => {
   app.use(expressValidator());
 
   // api router
-  app.use('/', routes);
+  app.use("/", routes);
 
   app.server.listen(process.env.PORT || config.port, () => {
-    console.log(`Started on port ${app.server.address().port}`);  // eslint-disable-line
+    console.log(`Started on port ${app.server.address().port}`); // eslint-disable-line
   });
 });
 
